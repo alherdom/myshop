d798598b48d62794925af0727ee7e7cc679cafae
Alejandro Hdez <alherdom@outlook.com>
2023-11-03 16:36:51 +0000

    Chapter 8 page 374

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -13,36 +14,34 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
-    
+
     def get_absolute_url(self):
-           return reverse('shop:product_list_by_category',
-                          args=[self.slug])
+        return reverse("shop:product_list_by_category", args=[self.slug])
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category,
-                                related_name='products',
-                                on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
-                                blank=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10,
-                                decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-       
+
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
         indexes = [
-            models.Index(fields=['id', 'slug']),
-            models.Index(fields=['name']),
-            models.Index(fields=['-created']),
-    ]
+            models.Index(fields=["id", "slug"]),
+            models.Index(fields=["name"]),
+            models.Index(fields=["-created"]),
+        ]
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-           return reverse('shop:product_detail',
-                          args=[self.id, self.slug])
+        return reverse("shop:product_detail", args=[self.id, self.slug])
