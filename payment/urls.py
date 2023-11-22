3523f2b0a57852258f134c7cd0dc19ae9946d525
Alejandro Hdez <alherdom@outlook.com>
2023-11-22 20:29:20 +0000

    Chapter 9 page 401

---
+++ payment/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "payment"
+
+urlspatterns = [
+    path("process/", views.payment_process, name="process"),
+    path("completed/", views.payment_completed, name="completed"),
+    path("canceled/", views.payment_canceled, name="canceled"),
+]
--- payment/views.py
+++ payment/views.py
@@ -10,9 +10,47 @@ from orders.models import Order
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
+
 def payment_process(request):
     order_id = request.session.get("order_id")
     order = get_object_or_404(Order, id=order_id)
 
     if request.method == "POST":
-        success_url = request.build_absolute_uri(reverse)
+        success_url = request.build_absolute_uri(reverse("payment:completed"))
+        cancel_url = request.build_absolute_uri(reverse("payment:canceled"))
+        # Striped checkout session data
+        session_date = {
+            "mode": "payment",
+            "client_reference_id": order_id,
+            "success_url": success_url,
+            "cancel_url": cancel_url,
+            "line_items": [],
+        }
+        # Add other items to the Stripe checkout session
+        for item in order.items.all():
+            session_data["line_items"].append(
+                {
+                    "price_date": {
+                        "unit_amount": int(item.price * Decimal("100")),
+                        "currency": "usd",
+                        "product_data": {
+                            "name": item.product.name,
+                        },
+                    },
+                    "quantity": item.quantity,
+                }
+            )
+        # Create Stripe checkout session
+        session = stripe.checkout.Session.create(**session_data)
+        # Redirect to Stripe payment form
+        return redirect(session.url, code=303)
+    else:
+        return render(request, "payment/process.html", locals())
+
+
+def payment_completed(request):
+    return render(request, "payment/completed.html")
+
+
+def payment_canceled(request):
+    return render(request, "payment/canceled.html")
