d5d4dff62e4a13910fa4205638f05434df486f61
Alejandro Hdez <alherdom@outlook.com>
2023-11-23 20:26:24 +0000

    Chapter 9 page 431

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
+import csv
+import datetime
+from django.http import HttpResponse
+
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ""
 
 
 class OrderItemInline(admin.TabularInline):
@@ -18,8 +30,34 @@ class OrderAdmin(admin.ModelAdmin):
         "postal_code",
         "city",
         "paid",
+        order_payment,
         "created",
         "updated",
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    action = [export_to_csv]
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.vebose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+    export_to_csv.short_description = "Export to CSV"
